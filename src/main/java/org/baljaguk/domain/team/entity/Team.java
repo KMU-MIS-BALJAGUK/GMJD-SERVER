eed883486b833c373f0953d731975a7337953637
koobontak <kbt82883@gmail.com>
2025-11-10 20:36:51 +0900

    feat: 팀 엔티티 작성

---
+++ src/main/java/org/baljaguk/domain/team/entity/Team.java
@@ -0,0 +1,49 @@
+package org.baljaguk.domain.team.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.baljaguk.domain.contest.entity.Contest;
+import org.baljaguk.domain.user.entity.User;
+import org.baljaguk.global.BaseEntity;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@Table(name = "teams")
+public class Team extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "introduction", nullable = false)
+    private String introduction;
+
+    @Column(name = "max_member", nullable = false)
+    private Integer maxMember;
+
+    @Column(name = "status", nullable = false)
+    private TeamStatus status;
+
+    @Column(name = "memo", nullable = true)
+    private String memo;
+
+    @Column(name = "question", nullable = false)
+    private String question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_leader_id", nullable = false)
+    private User teamLeader;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "contest_id", nullable = false)
+    private Contest contest;
+}

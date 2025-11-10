3ef63ba637077241aa0cfcdf3be05a98d4bc8bcd
koobontak <kbt82883@gmail.com>
2025-11-10 20:36:14 +0900

    init: 스프링 애플리케이션

---
+++ src/main/java/org/baljaguk/GmjdApplication.java
@@ -0,0 +1,13 @@
+package org.baljaguk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmjdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmjdApplication.class, args);
+    }
+
+}

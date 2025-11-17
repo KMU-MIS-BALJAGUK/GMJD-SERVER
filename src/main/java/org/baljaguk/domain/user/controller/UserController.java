e05bb2e3d79e2eb60ee11f8b14cd499fa6fcd3f9
koobontak <kbt82883@gmail.com>
2025-11-17 20:22:57 +0900

    feat: 테스트용 API 삭제

--- src/main/java/org/baljaguk/domain/user/controller/UserController.java
+++ src/main/java/org/baljaguk/domain/user/controller/UserController.java
@@ -1,13 +1,7 @@
 package org.baljaguk.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.baljaguk.domain.user.dto.CustomUserDetails;
-import org.baljaguk.domain.user.dto.response.UserRes;
 import org.baljaguk.domain.user.service.UserService;
-import org.baljaguk.global.api.ApiResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,15 +10,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    @GetMapping("/test")
-    public ResponseEntity<ApiResponse<Void>> getUser() {
-        return ResponseEntity.ok(ApiResponse.ok(null));
-    }
-
-    @GetMapping("/userTest")
-    public ResponseEntity<ApiResponse<UserRes>> getUserTest(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
-        UserRes response = userService.findUser(customUserDetails.getUser());
-        return ResponseEntity.ok(ApiResponse.ok(response));
-    }
 }
--- src/main/java/org/baljaguk/domain/user/service/UserService.java
+++ src/main/java/org/baljaguk/domain/user/service/UserService.java
@@ -1,8 +1,4 @@
 package org.baljaguk.domain.user.service;
 
-import org.baljaguk.domain.user.dto.response.UserRes;
-import org.baljaguk.domain.user.entity.User;
-
 public interface UserService {
-    UserRes findUser(User user);
 }

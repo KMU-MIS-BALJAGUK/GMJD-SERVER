bbe6f203509011b417ad7dee6d236ffd03224dda
koobontak <kbt82883@gmail.com>
2025-11-17 20:18:44 +0900

    feat: 구글 소셜 로그인 컨트롤러 레이어

---
+++ src/main/java/org/baljaguk/domain/user/controller/GoogleOAuthController.java
@@ -0,0 +1,55 @@
+package org.baljaguk.domain.user.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.baljaguk.domain.user.dto.response.JwtLoginResponse;
+import org.baljaguk.domain.user.service.GoogleAuthService;
+import org.baljaguk.global.api.ApiResponse;
+import org.baljaguk.global.security.properies.GoogleOAuthProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class GoogleOAuthController {
+
+    private final GoogleOAuthProperties googleOAuthProperties;
+    private final GoogleAuthService googleAuthService;
+
+    @GetMapping("/oauth/google")
+    @Operation(summary = "구글 소셜로그인 API",
+            description = "구글로 로그인 요청을 전송합니다.\n" +
+                    "그후 아래 API에 인가코드를 넣어 요청합니다.")
+    public ResponseEntity<Void> redirectToGoogleAuth() {
+        String redirectUri = googleOAuthProperties.getRegistration().getGoogle().getRedirectUri();
+        String clientId = googleOAuthProperties.getRegistration().getGoogle().getClientId();
+        String scope = String.join(" ", googleOAuthProperties.getRegistration().getGoogle().getScope());
+
+        String authUrl = "https://accounts.google.com/o/oauth2/v2/auth" +
+                "?client_id=" + clientId +
+                "&redirect_uri=" + redirectUri +
+                "&response_type=code" +
+                "&scope=" + scope +
+                "&access_type=offline" +
+                "&prompt=consent";
+
+        return ResponseEntity.status(HttpStatus.FOUND)  // 302
+                .header("Location", authUrl)
+                .build();
+    }
+
+    @GetMapping("/oauth/google/callback")
+    @Operation(summary = "구글 인증서버 토큰 검증 API",
+            description = "리다이렉트에서 AccessCode를 가지고 서버로 돌아오기 위한 엔드포인트입니다\n" +
+                    "해당 코드를 이용해서 사용자 정보를 파싱하고 액세스 토큰는 헤더에, 리프레시 토큰은 쿠키에 담아 반환합니다")
+    public ResponseEntity<ApiResponse<Void>> handleGoogleCallback(@RequestParam("code") String code) {
+        return googleAuthService.loginOrRegisterWithResponse(code);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ApiResponse<String>> logout() {
+        // 리프레시 토큰 무효화 로직이 필요하다면 여기에 구현
+        return ResponseEntity.ok(ApiResponse.ok("로그아웃이 정상적으로 처리되었습니다."));
+    }
+}

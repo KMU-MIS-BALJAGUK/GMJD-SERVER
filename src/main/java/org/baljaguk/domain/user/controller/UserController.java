9ca78c3c0e9de7c77ac8482b91a4474dbc5f34e5
koobontak <kbt82883@gmail.com>
2025-11-17 20:22:12 +0900

    feat: 테스트용 API 작성

--- src/main/java/org/baljaguk/domain/user/controller/UserController.java
+++ src/main/java/org/baljaguk/domain/user/controller/UserController.java
@@ -1,9 +1,12 @@
 package org.baljaguk.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.baljaguk.domain.user.dto.CustomUserDetails;
+import org.baljaguk.domain.user.dto.response.UserRes;
 import org.baljaguk.domain.user.service.UserService;
 import org.baljaguk.global.api.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +20,11 @@ public class UserController {
     @GetMapping("/test")
     public ResponseEntity<ApiResponse<Void>> getUser() {
         return ResponseEntity.ok(ApiResponse.ok(null));
+    }
+
+    @GetMapping("/userTest")
+    public ResponseEntity<ApiResponse<UserRes>> getUserTest(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        UserRes response = userService.findUser(customUserDetails.getUser());
+        return ResponseEntity.ok(ApiResponse.ok(response));
     }
 }
--- src/main/java/org/baljaguk/domain/user/repository/UserRepository.java
+++ src/main/java/org/baljaguk/domain/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package org.baljaguk.domain.user.repository;
 import org.baljaguk.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }

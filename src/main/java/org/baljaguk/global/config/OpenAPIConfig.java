bbbe45bb014d4174296a55901c3d392db2d8cde7
koobontak <kbt82883@gmail.com>
2025-11-18 01:07:44 +0900

    feat: swagger 서버 경로 수정

--- src/main/java/org/baljaguk/global/config/OpenAPIConfig.java
+++ src/main/java/org/baljaguk/global/config/OpenAPIConfig.java
@@ -1,5 +1,6 @@
 package org.baljaguk.global.config;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -17,10 +18,11 @@ public class OpenAPIConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Baljaguk API Docs")
-                        .description("발자국 API 명세서입니다.")
+                        .title("공모자들 API")
+                        .description("공모자들 API 명세서입니다.")
                         .version("v1.0"))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .addServersItem(new Server().url("/"))
                 .components(new Components()
                         .addSecuritySchemes(SECURITY_SCHEME_NAME,
                                 new SecurityScheme()

e0195e06db2b6a56f550334a81ee6ae160f0e299
koobontak <kbt82883@gmail.com>
2025-11-17 20:19:32 +0900

    feat: JWT 구성 요소 추가

--- src/main/java/org/baljaguk/global/security/properies/JwtProperties.java
+++ src/main/java/org/baljaguk/global/security/properies/JwtProperties.java
@@ -3,13 +3,14 @@ package org.baljaguk.global.security.properies;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
     private String secretKey;
     private long expireLength;
+    private long refreshExpireLength;
 }

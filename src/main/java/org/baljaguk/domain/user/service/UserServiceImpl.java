3830aa7dce2cc6e4c3143c6ee7c000fa526333db
koobontak <kbt82883@gmail.com>
2025-11-17 20:22:57 +0900

    feat: 테스트용 API 삭제

--- src/main/java/org/baljaguk/domain/user/service/UserServiceImpl.java
+++ src/main/java/org/baljaguk/domain/user/service/UserServiceImpl.java
@@ -1,8 +1,6 @@
 package org.baljaguk.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
-import org.baljaguk.domain.user.dto.response.UserRes;
-import org.baljaguk.domain.user.entity.User;
 import org.baljaguk.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,10 +10,4 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
-
-    @Override
-    public UserRes findUser(User user) {
-        userRepository.findById(user.getId());
-        return new UserRes(user.getId(), user.getName(), user.getEmail());
-    }
 }

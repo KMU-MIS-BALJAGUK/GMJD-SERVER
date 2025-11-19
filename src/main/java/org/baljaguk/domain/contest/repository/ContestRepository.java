2c838f98db2078588105165b0d7c0e002fab639a
koobontak <kbt82883@gmail.com>
2025-11-19 16:39:46 +0900

    refacto: 동적쿼리 쿼리dsl 리팩토링

--- src/main/java/org/baljaguk/domain/contest/repository/ContestRepository.java
+++ src/main/java/org/baljaguk/domain/contest/repository/ContestRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ContestRepository extends JpaRepository<Contest, Long> {
+public interface ContestRepository extends JpaRepository<Contest, Long>, ContestRepositoryCustom {
     @Query("""
         SELECT c FROM Contest c
         WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :keyword, '%'))

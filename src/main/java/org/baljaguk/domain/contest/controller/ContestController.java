eb336b797224b5fac4e45bba6267782c8dd61ce1
koobontak <kbt82883@gmail.com>
2025-11-19 12:07:01 +0900

    feat: 공모전 검색 API 구현

--- src/main/java/org/baljaguk/domain/contest/controller/ContestController.java
+++ src/main/java/org/baljaguk/domain/contest/controller/ContestController.java
@@ -2,13 +2,13 @@ package org.baljaguk.domain.contest.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.baljaguk.domain.contest.dto.response.ContestDetailResponse;
+import org.baljaguk.domain.contest.dto.response.ContestSearchResponse;
 import org.baljaguk.domain.contest.service.ContestService;
 import org.baljaguk.global.api.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/contests")
@@ -21,5 +21,10 @@ public class ContestController {
     public ResponseEntity<ApiResponse<ContestDetailResponse>> getContestDetail(@PathVariable Long contestId) {
         ContestDetailResponse detailResponse = contestService.getContestDetail(contestId);
         return ResponseEntity.ok(ApiResponse.ok(detailResponse));
+    }
+
+    @GetMapping("/api/contests/search")
+    public List<ContestSearchResponse> search(@RequestParam String keyword) {
+        return contestService.search(keyword);
     }
 }
--- src/main/java/org/baljaguk/domain/contest/service/ContestService.java
+++ src/main/java/org/baljaguk/domain/contest/service/ContestService.java
@@ -1,7 +1,12 @@
 package org.baljaguk.domain.contest.service;
 
 import org.baljaguk.domain.contest.dto.response.ContestDetailResponse;
+import org.baljaguk.domain.contest.dto.response.ContestSearchResponse;
+
+import java.util.List;
 
 public interface ContestService {
     ContestDetailResponse getContestDetail(Long contestId);
+
+    List<ContestSearchResponse> search(String keyword);
 }

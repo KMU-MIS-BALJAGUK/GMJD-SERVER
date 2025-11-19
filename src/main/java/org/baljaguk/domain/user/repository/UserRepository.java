58705a42a4388c50239ec6f1519f69a3e8ab7be7
koobontak <kbt82883@gmail.com>
2025-11-19 16:30:36 +0900

    feat: 마이프로필 조회 API 구현

--- src/main/java/org/baljaguk/domain/user/repository/UserRepository.java
+++ src/main/java/org/baljaguk/domain/user/repository/UserRepository.java
@@ -2,9 +2,18 @@ package org.baljaguk.domain.user.repository;
 
 import org.baljaguk.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Query("""
+        SELECT u FROM User u
+        LEFT JOIN FETCH u.categories uc
+        LEFT JOIN FETCH uc.category c
+        WHERE u.id = :userId
+    """)
+    Optional<User> findUserWithCategories(Long userId);
 }

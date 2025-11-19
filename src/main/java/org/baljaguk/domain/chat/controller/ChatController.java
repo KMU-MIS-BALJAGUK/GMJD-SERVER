c7129a3bbfff3cb7f6b06e006178c698629e8b62
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-11-20 03:28:09 +0900

    debug(websocket): stomp, amqp port 인식 오류 해결
    - yaml 파일 수정
    - build.gradle reactor-netty 의존성 추가
    - ChatController 코드 userdetail사용하도록 수정
    - WebsocketConfig yaml 파일 로딩 수정

--- src/main/java/org/baljaguk/domain/chat/controller/ChatController.java
+++ src/main/java/org/baljaguk/domain/chat/controller/ChatController.java
@@ -5,12 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.baljaguk.domain.chat.config.RabbitMqConfig;
 import org.baljaguk.domain.chat.entity.dto.ChatMessageDto;
 import org.baljaguk.domain.chat.entity.dto.ChatMessageRequest;
+import org.baljaguk.domain.user.dto.CustomUserDetails;
+import org.baljaguk.domain.user.entity.User;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.time.LocalDateTime;
 
 //STOMP (메시지 퍼블리싱) 컨트롤러
 @Slf4j
@@ -23,42 +27,35 @@ public class ChatController {
     /**
      * @param roomId  채팅방 ID(url)
      * @param message 클라이언트가 보낸 JSON 페이로드 (ChatMessageRequest)
-     * @param user Oauth2User객체에서 email 속성
+     * @param userDetails customUserDetail 타입의 user 객체
      * @return        @SendTo 경로로 브로드캐스팅될 JSON 페이로드 (ChatMessageDto)
      */
-//    @MessageMapping("/chat.room.{roomId}") // websocket 매핑
-//    @SendTo("/topic/chat.room.{roomId}") // STOMP(rabbitmq) 실시간 브로드캐스팅
-//    public ChatMessageDto sendMessage(@DestinationVariable Long roomId,
-//                                      ChatMessageRequest message,
-//                                      @AuthenticationPrincipal CustomUserDetails userDetails) {
-//
-//        if (user == null) {
-//            throw new IllegalArgumentException("로그인 정보가 없음");
-//        }
-//
-//        Object principalId = user.getAttribute("id");
-//
-//        long userId;
-//        if (principalId instanceof Integer) {
-//            userId = ((Integer) principalId).longValue();
-//        } else if (principalId instanceof Long) {
-//            userId = (Long) principalId;
-//        } else {
-//            throw new IllegalStateException("OAuth2User에서 유효한 사용자 ID(Long)를 추출할 수 없습니다.");
-//        }
-//
-//        //DTO 생성
-//        ChatMessageDto chatMessageDto = new ChatMessageDto();
-//
-//        // Queue로 message publishing (AMPQ)
-//        rabbitTemplate.convertAndSend(
-//                RabbitMqConfig.PERSISTENCE_QUEUE_NAME,
-//                chatMessageDto
-//        );
-//
-//        log.info("Message sent to AMQP queue for persistence.");
-//
-//        // websocket에 연결된 동일한 채널(roomId)으로 브로드 캐스팅(STOMP)
-//        return chatMessageDto;
-//    }
+    @MessageMapping("/chat.room.{roomId}") // websocket 매핑
+    @SendTo("/topic/chat.room.{roomId}") // STOMP(rabbitmq) 실시간 브로드캐스팅
+    public ChatMessageDto sendMessage(@DestinationVariable Long roomId,
+                                      ChatMessageRequest message,
+                                      @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        //user객체 생성
+        User user = userDetails.getUser();
+
+        //ChatMessageDto 생성
+        ChatMessageDto chatMessageDto = ChatMessageDto.builder()
+                .roomId(roomId)
+                .message(message.getMessage())
+                .userId(user.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        // Queue로 message publishing (AMPQ)
+        rabbitTemplate.convertAndSend(
+                RabbitMqConfig.PERSISTENCE_QUEUE_NAME,
+                chatMessageDto
+        );
+
+        log.info("Message sent to AMQP queue for persistence.");
+
+        // websocket에 연결된 동일한 채널(roomId)으로 브로드 캐스팅(STOMP)
+        return chatMessageDto;
+    }
 }

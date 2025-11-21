a655dfc86023c35c033566f444b06f4681b15c79
koobontak <kbt82883@gmail.com>
2025-11-21 16:23:03 +0900

    feat: team 목록 조회 api

--- src/main/java/org/baljaguk/domain/team/repository/TeamRepository.java
+++ src/main/java/org/baljaguk/domain/team/repository/TeamRepository.java
@@ -1,9 +1,12 @@
 package org.baljaguk.domain.team.repository;
 
+import org.baljaguk.domain.contest.entity.Contest;
 import org.baljaguk.domain.team.entity.Team;
 import org.baljaguk.domain.team.entity.TeamStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TeamRepository extends JpaRepository<Team, Long>, TeamRepositoryCustom {
+    List<Team> findByContestAndStatus(Contest contest, TeamStatus status);
 }
--- src/main/java/org/baljaguk/domain/team/service/TeamService.java
+++ src/main/java/org/baljaguk/domain/team/service/TeamService.java
@@ -3,9 +3,12 @@ package org.baljaguk.domain.team.service;
 import jakarta.validation.Valid;
 import org.baljaguk.domain.team.dto.request.CreateTeamRequest;
 import org.baljaguk.domain.team.dto.response.AIRecommendQuestionsResponse;
+import org.baljaguk.domain.team.dto.response.ContestTeamListResponse;
 
 public interface TeamService {
     AIRecommendQuestionsResponse getAIRecommendQuestions(Long contestId);
 
     void createTeam(Long userId, Long contestId, @Valid CreateTeamRequest request);
+
+    ContestTeamListResponse getTeamList(Long contestId);
 }

4b42067a3210e42a340b9c274b00cdb64ee84af5
koobontak <kbt82883@gmail.com>
2025-11-21 17:44:37 +0900

    feat: 테스터 아이디 변경

--- src/main/java/org/baljaguk/domain/user/service/JWTService.java
+++ src/main/java/org/baljaguk/domain/user/service/JWTService.java
@@ -16,7 +16,7 @@ public class JWTService {
     // 토큰 발급기를 위한 메서드입니다
     public void createToken(HttpServletResponse response) {
 
-        String access = jwtUtil.generateAccessToken(3L);
+        String access = jwtUtil.generateAccessToken(1L);
         response.setHeader("access-token", access);
     }
 }

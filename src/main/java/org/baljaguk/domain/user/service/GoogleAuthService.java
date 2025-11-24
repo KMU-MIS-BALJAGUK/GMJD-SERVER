4c72e2305e6aa9b4ae6899115f629dc1fa00068c
koobontak <kbt82883@gmail.com>
2025-11-24 16:30:15 +0900

    fix: 에러코드 및 유저 회원가입 상태 수정

--- src/main/java/org/baljaguk/domain/user/service/GoogleAuthService.java
+++ src/main/java/org/baljaguk/domain/user/service/GoogleAuthService.java
@@ -53,14 +53,12 @@ public class GoogleAuthService {
 
         // 2. DB에 유저 존재 여부 확인
         User user;
-        boolean isRegistered = true;
 
         Optional<User> optionalUser = userRepository.findByEmail(profile.email());
 
         if (optionalUser.isPresent()) {
             user = optionalUser.get();
         } else {
-            isRegistered = false;
             user = userRepository.save(
                     User.createSocialUser(profile.email(), profile.name(), profile.picture())
             );
@@ -71,7 +69,7 @@ public class GoogleAuthService {
         String serverRefreshToken = jwtUtil.generateRefreshToken(user.getId());
         log.info("🔑 JWT 발급 완료 - userId: {}, isRegistered: {}", user.getId(), user.isRegistered());
 
-        return JwtLoginResponse.of(user, serverAccessToken, serverRefreshToken, isRegistered);
+        return JwtLoginResponse.of(user, serverAccessToken, serverRefreshToken, user.isRegistered());
     }
 
     /**

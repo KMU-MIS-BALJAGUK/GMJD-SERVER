ab83702cdbdb1884ce2db5d36934056cafe5ee1b
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-12-03 21:29:05 +0900

    [FIX] 채팅(rabbitmq) config 작성
    - spring 내장 브로커로 변경
    - endpoint 유지

---
+++ src/main/java/org/baljaguk/domain/chat/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package org.baljaguk.domain.chat.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat")
+                .setAllowedOrigins("https://gmjd-web.vercel.app", "http://localhost:8080")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        //내장 브로커의 prefix
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
+}

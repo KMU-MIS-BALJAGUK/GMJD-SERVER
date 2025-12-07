4be195fd53037e03bb469d04a571a852afb1a4cc
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-12-08 00:34:23 +0900

    [FIX] 채팅(spring 내장 브로커 사용)
    - ChatController에 경로 상 roomId를 chatmessagedto의 필드로

--- src/main/java/org/baljaguk/domain/chat/controller/ChatController.java
+++ src/main/java/org/baljaguk/domain/chat/controller/ChatController.java
@@ -22,8 +22,13 @@ public class ChatController {
     @MessageMapping("/chat.room.{roomId}")
     public void sendMessage(@DestinationVariable("roomId")Long roomId, ChatMessageDto chatMessageDto) {
 
+        //roomId 지정
+        ChatMessageDto finalChatMessageDto = chatMessageDto.toBuilder()
+                .roomId(roomId)
+                .build();
+
         //DB저장
-        chatService.saveMessage(chatMessageDto);
+        chatService.saveMessage(finalChatMessageDto);
 
         //broadcasting
         simpMessagingTemplate.convertAndSend("/topic/chat.room/"+roomId, chatMessageDto);
--- src/main/java/org/baljaguk/domain/chat/entity/dto/ChatMessageDto.java
+++ src/main/java/org/baljaguk/domain/chat/entity/dto/ChatMessageDto.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Builder(toBuilder = true)
 public class ChatMessageDto {
 
     private Long roomId;

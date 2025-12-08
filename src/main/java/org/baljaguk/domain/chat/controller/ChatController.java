d5803c7a2c41c8c89dc669b79095421e1502c013
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-12-09 02:39:51 +0900

    [FIX] 채팅(spring 내장 브로커 사용)
    - 메시지 dto에 userId 추가

--- src/main/java/org/baljaguk/domain/chat/controller/ChatController.java
+++ src/main/java/org/baljaguk/domain/chat/controller/ChatController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.baljaguk.domain.chat.entity.dto.ChatMessageDto;
 import org.baljaguk.domain.chat.service.ChatService;
+import org.baljaguk.domain.user.dto.CustomUserDetails;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
 
@@ -20,11 +22,15 @@ public class ChatController {
     private final ChatService chatService;
 
     @MessageMapping("/chat.room.{roomId}")
-    public void sendMessage(@DestinationVariable("roomId")Long roomId, ChatMessageDto chatMessageDto) {
+    public void sendMessage(@DestinationVariable("roomId")Long roomId,
+                            ChatMessageDto chatMessageDto,
+                            @AuthenticationPrincipal CustomUserDetails userDetails) {
+
 
         //roomId 지정
         ChatMessageDto finalChatMessageDto = chatMessageDto.toBuilder()
                 .roomId(roomId)
+                .userId(userDetails.getUserId())
                 .build();
 
         //DB저장
